4b5a7400275bda530dc3ddd9bc7c056d2c82f010
Lais Tavares <alaisgomes@gmail.com>
2011-09-19 03:56:20 -0300

    att.

--- modulo_usuarios.c
+++ modulo_usuarios.c
@@ -1,6 +1,10 @@
 /*Arquivo destinado à implementação das funções referentes à manipulação de usuários.*/
 #include <stdio.h>
 #include <string.h>
+#include "modulo_arquivos.h"
+#include "modulo_interface.h"
+#include "modulo_itens.h"
+#include "modulo_pedidos.h"
 
 /***
  * IMPORTANTE: ATUALIZAR TODAS AS FUNCOES PARA AS PADRONIZADAS
@@ -10,15 +14,135 @@
 
 usuario *posicao_usuario;
 
+void InsereListaUsuarios(usuario **epinicio, usuario novo_usuario){
+	usuario *p1, *p2;
+
+	p1 = malloc (sizeof (usuario));
+	strcpy (p1->CPF, novo_usuario.CPF);
+	strcpy (p1->senha, novo_usuario.senha);
+	strcpy (p1->nome, novo_usuario.nome);
+	p1->tipo_acesso = novo_usuario.tipo_acesso;
+
+	if (*epinicio == NULL) {
+		*epinicio = p1;
+		p1->prox_usuario = NULL;
+	} else {
+	      p1->prox_usuario = *epinicio;
+	      *epinicio = p1;
+	}
+	p1 = NULL;
+}
+void alterarCPF (char *novo_cpf, usuario **pusuario_atual) {
+	strcpy (pusuario_atual->CPF, novo_cpf);
+}
+
+void alterarNome (char *novo_nome, usuario **pusuario_atual) {
+	strcpy (pusuario_atual->nome, novo_nome);
+}
+
+void alterarSenha (char *nova_senha, usuario **pusuario_atual) {
+	strcpy (pusuario_atual->CPF, nova_senha);
+}
+void apontaListaUsuarios (usuario **p_usuario_atual, char *cpf_desejado) {
+	usuario*p1;
+
+	p1 = *plista_usuarios;
+	while ((p1 != NULL) && ((strcmp(plista_usuarios->CPF, cpf_desejado))=! 0 )) {
+		p1 = p1->prox_usuario;
+	}
+	*p_usuario_atual=p1;
+}
+
+
+int verificaTipoUsuario (usuario **atual) {
+	int tipo;
+
+	tipo = *atual->tipo_acesso;
+	return tipo;
+}
+
+int pesquisaListaUsuarios (char *cpf_procura) {
+	usuario *p1;
+
+	p1 = *plista_usuarios;
+	while ((p1 != NULL) && ((strcmp(p1->CPF, cpf_procura))=! 0 )) {
+		p1 = p1->prox_usuario;
+	}
+	/*se p1 for NULL, chegou no fim da lista e o usuario nem existe, dai retorna 0, se nao, achou ele dai sim retorna 1, que ele existe */
+	if (p1 == NULL) {
+		return 0;
+	} else {
+		return 1;
+	}
+}
+void mostrarListaUsuarios (usuario **plista_usuarios) {
+	usuario *p1;
+
+	if (*plista_usuarios == NULL)
+		printf ("nenhum usuario cadastrado ainda \n");
+	else {
+		p1 = *plista_usuarios;
+    		while (p1 != NULL) {
+    			printf("%-11s %-50s\n", p1->CPF, p1->nome);
+    			p1 = p1->prox_usuario;
+    		}
+  	}
+}
+
+int procurarSenhaUsuarios (char *senha) {
+	usuario *p1;
+
+	p1 = *plista_usuarios;
+	while ((p1 != NULL) && ((strcmp(p1->senha, senha))=! 0 )) {
+		p1 = p1->prox_usuario;
+	}
+	/*se p1 for NULL, chegou no fim da lista e o usuario nem existe, dai retorna 0, se nao, achou ele dai sim retorna 1, que ele existe */
+	if (p1 == NULL) {
+		return 0;
+	} else {
+		return 1;
+	}
+}
+
+void deletarListaUsuarios (usuario **p_usuario_atual) {
+	usuario *p1, *p2;
+
+	if (*plista_usuarios == NULL)
+		printf("lista de usuarios vazia\n");
+	else if (*p_usuario_atual == NULL)
+		printf("usuario ja deletado anteriormente\n");
+	else {
+		p1 = *plista_usuarios;
+		while (p1 != NULL) {
+			if (p1->CPF == (*p_usuario_atual)->CPF) {
+				if (*plista_usuarios == p1){
+					*plista_usuarios = (*plista_usuarios)->prox_usuario;
+					free (p1);
+					p1 = *plista_usuarios;
+				} else {
+					p2->prox_usuario = p1->prox_usuario;
+					free (p1);
+					p1 = p2->prox_usuario;
+				}
+      			} else {
+        			p2 = p1;
+        			p1 = p1->prox_usuario;
+      			}
+		*p_usuario_atual = NULL; /*vai tipo deixar ele nulo, ja que eh global e tal, pra nao zoar */
+    		}
+	}
+}
+
+}
 /*FUNCAO PARA USUARIO REALLIZAR SUAS ATIVIDADES */
-int tarefaUsuarios (int tipo_usuario, usuario *posicao_usuario) {
+int tarefaUsuarios (int tipo_usuario, usuario **posicao_usuario) {
 
 	int operacao, codigo_item, continuar, numero_pedido, estado_pedido, codigo_item, tipoAlteracao;
-	int pedido_novo[60];
+	int pedido_novo[60], nova_quantidade;
 	item novo_item, *p_item;
 	float preco_item, novo_preco;
 	usuario *p_usuario_atual;
-	char novo_cpf[12], novo_nome[50], nova_senha[6];
+	char novo_cpf[12], novo_nome[50], nova_senha[6], nova_descricao[50];
 
 	/*criar os ponteiros alternativos aqui */
 	
@@ -37,7 +161,7 @@ int tarefaUsuarios (int tipo_usuario, usuario *posicao_usuario) {
 					break;
 					case 2: /*fazer um pedido REVER*/
 						do {
-							menuInserirPedido (&novo_pedido);
+							/*menuInserirPedido (&novo_pedido);*/
 							/*inserirPedido (lista de itens aserem inseridos no pedido , ponteiro do usuario para insetir os pedidos); */
 							continuar = funcaoContinuar ();
 
@@ -72,7 +196,7 @@ int tarefaUsuarios (int tipo_usuario, usuario *posicao_usuario) {
 				break;
 				case 2: /*fazer um pedido REVER*/
 					do {
-						menuInserirPedido (&novo_pedido);
+						/*menuInserirPedido (&novo_pedido); */
 						/*inserirPedido (lista de itens aserem inseridos no pedido, ponteiro do usuario para insetir os pedidos); */
 						continuar = funcaoContinuar ();
 
@@ -100,7 +224,7 @@ int tarefaUsuarios (int tipo_usuario, usuario *posicao_usuario) {
 							insereNome(&novo_nome);
 							alterarNome (novo_nome, &p_usuario_atual);
 						} else if (tipoAlteracao==4) { /*excluir*/
-							deletarListaUsuarios (&p_usuario_atual, /&plista_usuarios);
+							deletarListaUsuarios (&p_usuario_atual);
 						}
 						continuar = funcaoContinuar ();
 					}while (continuar == 1);
@@ -129,7 +253,7 @@ int tarefaUsuarios (int tipo_usuario, usuario *posicao_usuario) {
 							break;
 							case 4: /*alterar qtd de um item no estoque */
 								codigo_item = menuItem();
-								menuAlterarQtd (&nova_quantidade);
+								nova_quantidade = menuAlterarQtd ();
 								apontaListaItens(codigo_item, &p_item);
 								alterarQtdItem (nova_quantidade, &p_item);
 							break;
@@ -160,7 +284,7 @@ int FazerLogin (char *cpf,  char *nome, char *senha) {
 	
 
 	apontaListaUsuario(&posicao_usuario);
-	usuario_existe = pesquisaListaUsuarios (&cpf);
+	usuario_existe = pesquisaListaUsuarios (cpf);
 	senha_correta = procurarSenhaUsuario (*senha, &posicao_usuario);
 	if ((senha_correta==1)&&(usuario_existe==1)){
 		tipo_usuario = verificarTipoUsuario (posicao_usuario);
